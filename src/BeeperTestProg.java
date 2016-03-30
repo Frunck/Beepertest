0e115e0d2de0a52f338b166ca7c92014b5255d4f
Frunck <ay.kharchenko@gmail.com>
2016-03-30 21:04:23 +0300

    second commit

--- src/BeeperTestProg.java
+++ src/BeeperTestProg.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class BeeperTestProg {
+public class BeeperTestProg extends ConsoleProgram{
+
+	public void run(){
+		int[] arr = new int[4];
+		readArray(arr);
+		printArray(arr);
+	}
+
+	private void readArray(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			a[i] = readInt("?: ");
+		}
+	}
+
+	private static void printArray(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
 
 }
